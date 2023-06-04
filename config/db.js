22c40a0c289dd0e4245bba49293953aae8d3bb5e
Rajan Lamba <rk1759593@gmail.com>
2023-06-05 02:43:18 +0530

    remove html

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const env = require("dotenv").config();
 mongosse
   // .connect("mongodb://127.0.0.1:27017/issue-tracker")
   .connect(
-    `mongodb+srv://issue-tracker:${process.env.DB_KEY}@cluster0.7jjb7bp.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://issue-tracker:XPBIFzUpug4FhLSW@cluster0.7jjb7bp.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
     console.log("db conected");
